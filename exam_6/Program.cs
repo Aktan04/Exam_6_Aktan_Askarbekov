d351fa5c684077e0d04074d5d09f3ec9b57d482a
Askarbekov Aktan <akn392204@gmail.com>
2024-03-30 18:55:36 +0600

    added code in Main and some functions

---
+++ exam_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using exam_6;
+
+string site = "../../../site";
+List<TaskModel> tasks =GetTasksFromJson();
+DumpHttpServer server = new DumpHttpServer(tasks);
+
+await server.RunAsync(site, 8888);
+
+List<TaskModel> GetTasksFromJson()
+{
+    try
+    {
+        List<TaskModel> loadedTasks = MySerializer.LoadTasks();
+        if (loadedTasks != null && loadedTasks.Count > 0)
+            return loadedTasks;
+        else
+            return new List<TaskModel>();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        return new List<TaskModel>();
+    }
+    
+}
